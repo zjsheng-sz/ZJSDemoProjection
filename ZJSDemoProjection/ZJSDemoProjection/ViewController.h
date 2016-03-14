e2faa27f1c9a439054ca3b498e706acbd440ea11
zjsrose <770649662@qq.com>
2016-03-14 10:23:59 +0800

    creat a program

---
+++ ZJSDemoProjection/ZJSDemoProjection/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZJSDemoProjection
+//
+//  Created by ipi on 16/3/14.
+//  Copyright © 2016年 zjs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ZJSDemoProjection/ZJSDemoProjection/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZJSDemoProjection
+//
+//  Created by ipi on 16/3/14.
+//  Copyright © 2016年 zjs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
